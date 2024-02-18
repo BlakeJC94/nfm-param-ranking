2617aa2e1491948a4ce5645a5fd58be48cb1a74d
BlakeJC94 <blakejamescook@gmail.com>
2024-02-18 20:56:09 +1100

    Added TODOs

--- src/AnalyseResults.jl
+++ src/AnalyseResults.jl
@@ -1,5 +1,11 @@
 module AnalyseResults
 
+# TODO Load data
+# TODO Classify simulations
+# TODO Partition space with random forest of 100 trees
+# TODO Calculate normalised variable importance for each class
+# TODO Print results
+
 function main()
     println("BAZ!")
 end
--- src/GenerateData.jl
+++ src/GenerateData.jl
@@ -1,5 +1,10 @@
 module GenerateData
 
+# TODO Define equation
+# TODO Generate hypercube
+# TODO Solve each equation and save results to file
+# TODO Skip if all results have been generated
+
 function main()
     println("BAR!")
 end
