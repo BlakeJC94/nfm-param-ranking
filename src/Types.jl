9a0c8c78096c368456fe1481f777f91457c78ec9
BlakeJC94 <blake.c@montu.com.au>
2025-05-10 08:48:14 +1000

    refactor(Utils): save/load

--- src/Types.jl
+++ src/Types.jl
@@ -1,6 +1,8 @@
 module Types
 export ParameterRange, SimulationConfig, ModelConfig, Config
 
+import Base: ==
+
 using Parameters
 
 @with_kw struct ParameterRange
@@ -9,18 +11,32 @@ using Parameters
     max::Float64
 end
 
+function ==(a::ParameterRange, b::ParameterRange)
+    a.name == b.name && a.min == b.min && a.max == b.max
+end
+
 @with_kw struct SimulationConfig
     T0::Float64  # Transient end time (discard signal up to this t value)
     T::Float64   # Max simulation time
     Fs::Float64  # Sampling frequency
 end
 
+function ==(a::SimulationConfig, b::SimulationConfig)
+    a.T0 == b.T0 && a.T == b.T && a.Fs == b.Fs
+end
 
 @with_kw struct Config
     N::Int64
     simulation_config::SimulationConfig
     param_ranges::Vector{ParameterRange}
 end
+
+function ==(a::Config, b::Config)
+    a.N == b.N &&
+    a.simulation_config == b.simulation_config &&
+    a.param_ranges == b.param_ranges
+end
+
 
 @with_kw struct ModelConfig
     name::String
--- src/Utils.jl
+++ src/Utils.jl
@@ -18,6 +18,35 @@ function print_output(output::Dict{String, Vector{Tuple{String,Float64}}})
     end
 end
 
+
+
+function save_config(file::HDF5.File, config::Config)
+    g = create_group(file, "config")
+
+    g["N"] = config.N
+    g["simulation/T0"] = config.simulation_config.T0
+    g["simulation/T"]  = config.simulation_config.T
+    g["simulation/Fs"] = config.simulation_config.Fs
+
+    # Store parameter ranges as a dataset of strings/floats
+    names = [string(p.name) for p in config.param_ranges]
+    mins = [p.min for p in config.param_ranges]
+    maxs = [p.max for p in config.param_ranges]
+
+    g["param_ranges/names"] = names
+    g["param_ranges/mins"] = mins
+    g["param_ranges/maxs"] = maxs
+end
+
+function save_simulations(file::HDF5.File, simulations::Vector{Vector{Float64}})
+    sim_group = create_group(file, "simulations")
+
+    n_digits = length(string(length(simulations)))
+    for (i, sim) in enumerate(simulations)
+        sim_group[lpad(i,n_digits,"0")] = sim
+    end
+end
+
 function save_results(
     results_path::String,
     config::Config,
@@ -28,70 +57,54 @@ function save_results(
     h5open(results_path, "w") do file
         file["param_configs"] = param_configs
         file["labels"] = labels
-        sim_group = create_group(file, "simulations")
-
-        n_digits = length(string(length(simulations)))
-        for (i, sim) in enumerate(simulations)
-            sim_group[lpad(i,n_digits,"0")] = sim
-        end
-
-        g = create_group(file, "config")
-
-        g["N"] = config.N
-        g["simulation/T0"] = config.simulation_config.T0
-        g["simulation/T"]  = config.simulation_config.T
-        g["simulation/Fs"] = config.simulation_config.Fs
-
-        # Store parameter ranges as a dataset of strings/floats
-        names = [string(p.name) for p in config.param_ranges]
-        mins = [p.min for p in config.param_ranges]
-        maxs = [p.max for p in config.param_ranges]
-
-        g["param_ranges/names"] = names
-        g["param_ranges/mins"] = mins
-        g["param_ranges/maxs"] = maxs
+        save_simulations(file, simulations)
+        save_config(file, config)
     end
 end
+
+function load_config(file::HDF5.File)::Config
+    g = file["config"]
+
+    N = read(g["N"])
+    T0 = read(g["simulation/T0"])
+    T = read(g["simulation/T"])
+    Fs = read(g["simulation/Fs"])
+    sim_config = SimulationConfig(T0=T0, T=T, Fs=Fs)
+
+    names = Symbol.(read(g["param_ranges/names"]))
+    mins = read(g["param_ranges/mins"])
+    maxs = read(g["param_ranges/maxs"])
+
+    param_ranges = [ParameterRange(name=names[i], min=mins[i], max=maxs[i])
+                    for i in eachindex(names)]
+
+    return Config(N=N, simulation_config=sim_config, param_ranges=param_ranges)
+end
+
+function load_simulations(file::HDF5.File)::Vector{Vector{Float64}}
+    sim_group = file["simulations"]
+    n_sims = length(keys(sim_group))
+    simulations = Vector{Vector{Float64}}(undef, n_sims)
+
+    for (i, name) in enumerate(sort(collect(keys(sim_group))))
+        simulations[i] = read(sim_group[name])
+    end
+    return simulations
+end
+
 
 function load_results(
     results_path::String,
 )::Tuple{Config, Matrix{Float64}, Matrix{Float64}, Vector{Vector{Float64}}}
     @info "loading results"
-
-    param_configs = Matrix{Float64}(undef, 0, 0)
-    labels = Matrix{Float64}(undef, 0, 0)
-    simulations = Vector{Vector{Float64}}()
-
-    h5open(results_path, "r") do file
+    config, param_configs, labels, simulations = h5open(results_path, "r") do file
         param_configs = read(file["param_configs"])
         labels = read(file["labels"])
-
-        sim_group = file["simulations"]
-        n_sims = length(keys(sim_group))
-        simulations = Vector{Vector{Float64}}(undef, n_sims)
-
-        for (i, name) in enumerate(sort(collect(keys(sim_group))))
-            simulations[i] = read(sim_group[name])
-        end
-
-        g = file["config"]
-
-        N = read(g["N"])
-        T0 = read(g["simulation/T0"])
-        T = read(g["simulation/T"])
-        Fs = read(g["simulation/Fs"])
-        sim_config = SimulationConfig(T0=T0, T=T, Fs=Fs)
-
-        names = Symbol.(read(g["param_ranges/names"]))
-        mins = read(g["param_ranges/mins"])
-        maxs = read(g["param_ranges/maxs"])
-
-        param_ranges = [ParameterRange(name=names[i], min=mins[i], max=maxs[i])
-                        for i in eachindex(names)]
-
-        config = Config(N=N, simulation_config=sim_config, param_ranges=param_ranges)
+        simulations = load_simulations(file)
+        config = load_config(file)
         return config, param_configs, labels, simulations
     end
+    return config, param_configs, labels, simulations
 end
 
 end  # module Utils
